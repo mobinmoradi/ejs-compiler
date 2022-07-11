b8a91583273208a28eb22960177c3d0cb8a01326
mobin moradi <mobinmoradimail@gmail.com>
2022-07-11 13:03:43 +0430

    create temp1 & temp2

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const path = require('path');
 const fs = require('fs');
 
-const Handlebars = require("handlebars");
+const ejs = require("ejs");
 
 
-let filename = 'temp1';
+let filename = 'temp2';
 
-let source = fs.readFileSync(path.join(__dirname,'templates', `${filename}.handlebars`))
-let template = Handlebars.compile(source.toString('utf8'));
+let source = fs.readFileSync(path.join(__dirname,'templates', `${filename}.ejs`))
+let template = ejs.compile(source.toString('utf8'));
 
 let data ={
-    'name':'test',
-    'link':'google.com'
+    'url':'https://www.google.com',
+    'past':{
+      'status':'200',
+      'date':'1401/4/14 8:00:00'
+    },
+    'now':{
+      'status':'504',
+      'date':'1401/4/14 9:00:00'
+    }
 }
 
 
@@ -24,3 +31,5 @@ fs.writeFile(`${filename}.html`, result, (err) => {
       console.log(`${filename} is ready!`);
     }
 });
+
+
